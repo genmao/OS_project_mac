853da7d86e3b057e95e8770dbad82a27752b25f2
ZengFan92 <wuditharll@gmail.com>
2017-04-26 21:41:35 -0700

    Add std calculation and some loops
    note that process and thread only use 100 iters

--- Test_CPU.c
+++ Test_CPU.c
@@ -4,27 +4,52 @@
 #include <sys/types.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <math.h>
 
 
 #define ITERATION 100
+
 static __inline__ uint64_t rdtsc(void) {
     unsigned hi, lo;
     __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
     return ( (uint64_t)lo)|( ((uint64_t)hi)<<32 );
 }
 
+// use assembly MACRO to read RDTSC to avoid procedure call overhead
+#define read_start_time(hi,lo) \
+asm volatile ("CPUID\n\t" \
+"RDTSC\n\t" \
+"mov %% edx, %0\n\t" \
+"mov %% eax, %1\n\t": "=r" (hi), "=r" (lo):: "%rax","%rbx", "%rcx", "%rdx")
+
+#define read_end_time(hi,lo) \
+asm volatile ("RDTSC\n\t" \
+"mov %% edx, %0\n\t" \
+"mov %% eax, %1\n\t" \
+"CPUID\n\t": "=r" (hi), "=r" (lo):: "%rax","%rbx", "%rcx", "%rdx")
+
+
 //1: Measurement overhead
 //1-1: Overhead of reading time
 static double ReadTimeOverhead() {
     uint64_t start;
     uint64_t end;
-    double sum = 0;
+    double val [1000] = {0};
+    double sum = 0, std = 0;
     for (int i = 0; i < 1000; i++) {
         start = rdtsc();
         end = rdtsc();
+        val[i] = end - start;
         sum += end - start;
     }
     sum /= 1000.0;
+    
+    // calculate std
+    for (int i = 0; i<1000; i++){
+        std += (sum-val[i])*(sum-val[i]);
+    }
+    std = sqrt(std/1000);
+    printf("1000 iters: Read Time overhead std = %lf cycles\n", std);
     return sum;
 }
 
@@ -32,7 +57,8 @@ static double ReadTimeOverhead() {
 static double LoopOverhead() {
     uint64_t start;
     uint64_t end;
-    long double sum = 0;
+    double val [1000] = {0};
+    long double sum = 0, std = 0;
     int i,j;
     for (int i = 0; i < 1000; i++) {
         start = rdtsc();
@@ -40,9 +66,18 @@ static double LoopOverhead() {
             //Empty loop, the only operation is inc(j)
         }
         end = rdtsc();
+        val[i] = end - start;
         sum += (end - start) / 10000.0;
     }
     sum /= 1000.0;
+    
+    // calculate std
+    for (int i = 0; i<1000; i++){
+        std += (sum-val[i])*(sum-val[i]);
+    }
+    std = sqrt(std/1000);
+    printf("1000 iters: loop overhead std = %lf cycles\n", std);
+
     return sum;
 }
 
@@ -55,9 +90,11 @@ static __attribute__ ((noinline)) void TestArgument_4(int a, int b, int c, int d
 static __attribute__ ((noinline)) void TestArgument_5(int a, int b, int c, int d, int e) {}
 static __attribute__ ((noinline)) void TestArgument_6(int a, int b, int c, int d, int e, int f) {}
 static __attribute__ ((noinline)) void TestArgument_7(int a, int b, int c, int d, int e, int f, int g) {}
+
 static double ProcedureOverhead(int num) {
     uint64_t start, end;
-    double sum = 0;
+    double sum = 0, std = 0;
+    double val[1000] = {0};
     for (int i = 0; i < 1000; i++) {
         switch (num) {
             case 0:
@@ -193,8 +230,16 @@ static double ProcedureOverhead(int num) {
                 end = rdtsc();
         }
         sum += (end - start) / 1000.0;
+        val[i] = (end - start) / 1000.0;
     }
     sum /= 1000.0;
+    
+    // calculate std
+    for (int i = 0; i<1000; i++){
+        std += (sum-val[i])*(sum-val[i]);
+    }
+    std = sqrt(std/1000);
+    printf("Procedure call overhead with %d paras std = %lf cycles\n", num, std);
     return sum;
 }
 
@@ -202,43 +247,61 @@ static double ProcedureOverhead(int num) {
 static double SystemOverhead() {
     uint64_t start;
     uint64_t end;
-    double sum = 0;
+    double sum = 0, std = 0;
+    double val[1000] = {0};
     for (int i = 0; i < 1000; i++) {
         start = rdtsc();
         syscall(SYS_getpid);
         end = rdtsc();
         sum += end - start;
+        val[i] = end - start;
     }
     sum /= 1000.0;
+    // calculate std
+    for (int i = 0; i<1000; i++){
+        std += (sum-val[i])*(sum-val[i]);
+    }
+    std = sqrt(std/1000);
+    printf("System call overhead std = %lf cycles\n", std);
     return sum;
 }
 
 //4: Task creation time
 static double TaskCreationTime() {
-    double sum = 0;
-    for(int i = 0; i < ITERATION; i++){
-        uint64_t start, end;
+    uint64_t start, end;
+    double sum = 0, std = 0;
+    double val[1000] = {0};
+    //start = rdtsc();
+    for(int i = 0; i < 100; i++){
         start = rdtsc();
         pid_t pid = fork();
         end = rdtsc();
         if (pid == 0){
             exit(0);
         }
+        // parent process
         else{
-            //wait(NULL);
-            //end = rdtsc();
             sum += end - start;
+            val[i] = end - start;
         }
+        
     }
-    return sum / ITERATION;
+    //end = rdtsc();
+    sum /= 100.0;
+    for (int i = 0; i<100; i++){
+        std += (sum-val[i])*(sum-val[i]);
+    }
+    std = sqrt(std/100);
+    printf("Process creation overhead std = %lf cycles\n", std);
+    return sum;
+    
 }
 
 //5: Context switch time
-uint64_t OneSwitchTime(int *fd) {
-    uint64_t start, end, elapsed = 0;
-
+uint64_t SwitchTime(int *fd) {
+    uint64_t start, end;
+    uint64_t sum = 0;
     pid_t pid = fork();
-
     if (pid == 0){
         end = rdtsc();
         write(fd[1], (void*)&end, sizeof(end));
@@ -250,46 +313,50 @@ uint64_t OneSwitchTime(int *fd) {
         read(fd[0], (void*)&end, sizeof(end));
     }
     if(end > start){
-        elapsed = end - start;
+        sum = end - start;
     }
-    return elapsed;
+    return sum;
 }
 
-static double ProcessContextSwitchOverhead() {
-
+static double ContextSwitchOverhead() {
     int fd[2];
     pipe(fd);
-    uint64_t total_time = 0;
+    uint64_t sum = 0;
     int i = 0;
-    while(i < ITERATION) {
-        uint64_t elapsed = OneSwitchTime(fd);
-        //printf("%llu\n", elapsed);
-        if (elapsed > 0) {
-            total_time += elapsed;
-            i += 1;
+    uint64_t val[100] = {0};
+    double std = 0;
+    while(i < 100) {
+        uint64_t switchtime = SwitchTime(fd);
+        if (switchtime > 0) {
+            sum += switchtime;
+            val[i] = switchtime;
+            i++;
         }
     }
-    return (double)total_time/ (double)ITERATION;
+    double mean = (double)sum/ 100.0;
+    for(int i=0; i<100; i++){
+        std += (mean-(double)val[i])*(mean-(double)val[i]);
+    }
+    std = sqrt(std/100);
+    printf("Process context switch overhead std = %lf cycles\n", std);
+    return mean;
+
 }
 
 
-static double PipeOverhead() {
-    uint64_t total_time = 0;
-
-    int i = 0;
-    for (i = 0; i< ITERATION; i++){
-        uint64_t  start, end, elapsed;
+static double PipeAndRWOverhead() {
+    uint64_t sum = 0;
+    for (int i = 0; i < 100; i++){
+        uint64_t start, end;
         int fd[2];
         pipe(fd);
-
         start = rdtsc();
         write(fd[1], &start, sizeof(start));
         read(fd[0], &start, sizeof(start));
         end = rdtsc();
-        elapsed = end - start;
-        total_time += elapsed;
+        sum += end - start;
     }
-    return (double)total_time / ITERATION;
+    return (double)sum / 100.0;
 }
 
 
@@ -297,27 +364,41 @@ static double PipeOverhead() {
 void *Task() {
     pthread_exit(0);
 }
+
+//1-4-2 Kernel Switch time
 static double KernelOverhead() {
-    pthread_t thread;
-    uint64_t start;
-    uint64_t end;
-    uint64_t elapsed;
-    start = rdtsc();
-    pthread_create(&thread, NULL, Task, NULL);
-    end = rdtsc();
-    elapsed = (end - start);
-    return elapsed;
+    uint64_t start, end, sum = 0;
+    double std = 0;
+    double val[100] = {0};
+    for(int i=0; i<100; i++){
+        pthread_t thread;
+        start = rdtsc();
+        pthread_create(&thread, NULL, Task, NULL);
+        pthread_join(thread,NULL);
+        end = rdtsc();
+        sum += end - start;
+        val[i] = end - start;
+    }
+    
+    double mean = (double)sum/ 100.0;
+    for(int i=0; i<100; i++){
+        std += (mean-val[i])*(mean-val[i]);
+    }
+    std = sqrt(std/100);
+    printf("Kernel thread creation overhead std = %lf cycles\n", std);
+    return mean;
 }
 
 //Context switch kernel thread overhead
 static uint64_t thread_end;
 static uint64_t thread_start;
-static uint64_t cs_time;
+static uint64_t sum;
+static uint64_t val[100];
 void *Thread2() {
     thread_end = rdtsc();
     pthread_exit(0);
-
 }
+
 void *Thread1() {
     pthread_t t2;
     pthread_create(&t2, NULL, &Thread2, NULL);
@@ -325,49 +406,61 @@ void *Thread1() {
     pthread_join(t2, NULL);
     pthread_exit(0);
 }
-static double CSKernelOverhead() {
-    pthread_t t1;
-    pthread_create(&t1, NULL, &Thread1, NULL);
-    pthread_join(t1, NULL);
-    cs_time = thread_end - thread_start;
-    return cs_time;
+static double ContextSwitchTimeKernel() {
+    for(int i=0;i<100;i++){
+        pthread_t t1;
+        pthread_create(&t1, NULL, &Thread1, NULL);
+        pthread_join(t1, NULL);
+        sum += thread_end - thread_start;
+        val[i] = thread_end - thread_start;
+    }
+    
+    double mean = (double)sum/ 100.0;
+    double std = 0;
+    for(int i=0; i<100; i++){
+        std += (mean-(double)val[i])*(mean-(double)val[i]);
+    }
+    std = sqrt(std/100);
+    printf("kernel thread context switch overhead std = %lf cycles\n", std);
+    return mean;
 }
+
 int main(int argc, const char * argv[]) {
     double overhead;
-    int i = 0;
 
     //Read time overhead
     overhead = ReadTimeOverhead();
-    printf("Read Time overhead = %lf circles\n", overhead);
+    printf("Read Time overhead Ave. = %lf cycles\n", overhead);
 
     //Read time overhead
     overhead = LoopOverhead();
-    printf("Loop overhead = %lf circles\n", overhead);
+    printf("Loop overhead Ave. = %lf cycles\n", overhead);
 
     //Procedure call overhead
-    for (i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
         overhead = ProcedureOverhead(i);
-        printf("Procedure call overhead with %d arguments = %lf circles\n", i, overhead);
+        printf("Procedure call overhead with %d arguments Ave. = %lf cycles\n", i, overhead);
     }
     //System call overhead
     overhead = SystemOverhead();
-    printf("System call overhead = %lf circles\n", overhead);
+    printf("System call overhead Ave. = %lf cycles\n", overhead);
 
     //Task creation time
     overhead = TaskCreationTime();
-    printf("Process creation overhead = %lf circles\n", overhead);
-
-    //Context switch time
-    //printf("%lf\n", PipeOverhead());
-    overhead = ProcessContextSwitchOverhead()-PipeOverhead();
-    printf("Process context switch overhead = %lf circles\n", overhead);
+    printf("Process creation overhead Ave. = %lf cycles\n", overhead);
 
     //kernel thread
     overhead = KernelOverhead();
-    printf("kernel thread overhead = %lf circles\n", overhead);
+    printf("kernel thread creation overhead Ave. = %lf cycles\n", overhead);
+    
+    //Context switch time between process
+    //printf("%lf\n", PipeOverhead());
+    // Since we have 2 context switch, we need to devide by 2?
+    overhead = (ContextSwitchOverhead()-PipeAndRWOverhead()) ;
+    printf("Process context switch overhead Ave. = %lf cycles\n", overhead);
 
-    //kernel thread context switch time
-    overhead = CSKernelOverhead();
-    printf("kernel thread context switch overhead = %lf circles\n", overhead);
+    //Context switch time between kernel
+    overhead = ContextSwitchTimeKernel();
+    printf("kernel thread context switch overhead Ave. = %lf cycles\n", overhead);
     return 0;
 }
